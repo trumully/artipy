2044744846f149c20a7309312eb722a34d8f16e5
trumully <truman.mulholland@gmail.com>
2024-04-12 15:34:32 +1200

    docs: correct example.py with up-to-date build syntax

--- example.py
+++ example.py
@@ -1,16 +1,14 @@
 from artipy.artifacts import ArtifactBuilder
-from artipy.stats import MainStat, SubStat, StatType
+from artipy.stats import StatType
 
 
 def main() -> None:
     artifact = (
         ArtifactBuilder()
-        .with_mainstat(MainStat(StatType.ATK, 46))
-        .with_substat(SubStat(StatType.CRIT_RATE, 0.039))
-        .with_substat(SubStat(StatType.CRIT_DMG, 0.078))
-        .with_substats(
-            [SubStat(StatType.ATK_PERCENT, 0.05), SubStat(StatType.DEF_PERCENT, 0.05)]
-        )
+        .with_mainstat(StatType.ATK, 46)
+        .with_substat(StatType.CRIT_RATE, 0.039)
+        .with_substat(StatType.CRIT_DMG, 0.078)
+        .with_substats([(StatType.ATK_PERCENT, 0.05), (StatType.DEF_PERCENT, 0.05)])
         .with_level(0)
         .with_rarity(5)
         .with_set("Gladiator's Finale")
